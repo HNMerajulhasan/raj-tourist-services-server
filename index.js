d77b30ebb4d982afdd2891bfc32ab9f99e5feb55
Meraj <merajulhasan150@gmail.com>
2022-11-10 23:32:10 +0600

    2nd part index upload

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('raj-tourist-service server is running');
 })
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0vbsoxh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+      const serviceCollection=client.db('touristServices').collection('services')
+      const reviewCollection=client.db('touristServices').collection('reviews');
+
+       app.get('/services',async(req,res)=>{
+        const query={}
+        const cursor=serviceCollection.find(query);
+        const services=await cursor.limit(3).toArray();
+        res.send(services);
+     });
