030879dd9a9632599b823c213b04b1c6b47849af
Meraj <merajulhasan150@gmail.com>
2022-11-10 23:40:43 +0600

    mongodb added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/',(req,res)=>{
     res.send('raj-tourist-service server is running');
 })
 
-
+//mongodb added
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0vbsoxh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
