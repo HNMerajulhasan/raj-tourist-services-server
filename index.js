142f9f019c88ab452ad0c223018b5878650e8d01
Meraj <merajulhasan150@gmail.com>
2022-11-24 12:00:17 +0600

    Full server side deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { application } = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-//using middleware
+//Used Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -14,7 +14,7 @@ app.get('/',(req,res)=>{
     res.send('raj-tourist-service server is running');
 })
 
-//mongodb added
+//mongodb Database added
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0vbsoxh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
